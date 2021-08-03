414d56945055dc0d588b761790d6ca9c90a4fa49
Marcos Trejo <marcostrejo23@gmail.com>
2021-08-03 11:30:50 -0700

    pulling new files

---
+++ models/index.js
@@ -0,0 +1,29 @@
+const Photos = require('./Photos');
+const Reviews = require('./Reviews');
+const User = require('./User');
+const FriendsList = require('./friendslist');
+
+
+User.hasMany(Photos,{
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE',
+});
+
+User.hasMany(Reviews,{
+    foreignKey: 'user_ud',
+    onDelete: 'CASCADE',
+});
+
+User.hasOne(Friendslist,{
+    foreignKey: 'user_id',
+});
+
+Photos.BelongTo(User, {
+    foreignKey: 'user_id',
+});
+
+FriendsList.BelongsTo(User, {
+    foreignKey: 'user_id',
+});
+
+module.exports = { User, Reviews, FriendsList, Photos };
