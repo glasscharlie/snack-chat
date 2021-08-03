9afe5f5eb54ac8a983b64b55698581bab98c99e0
Charles Glass <charlie-glass@live.com>
2021-08-03 11:31:17 -0700

    starting user routes

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const bcrypt = require("bcrypt");
-// const db = require('../../models');
+const db = require('../../models');
 
 
 module.exports = router;
+
+router.post('/', (req/res) => {
+    db.User.create(req.body)
+    .then
+})
