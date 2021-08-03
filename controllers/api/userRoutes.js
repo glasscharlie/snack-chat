ead047c9f511be54701554b17fecc336fca1f3a3
Charles Glass <charlie-glass@live.com>
2021-08-03 11:50:42 -0700

    create user route

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -6,7 +6,16 @@ const db = require('../../models');
 
 module.exports = router;
 
-router.post('/', (req/res) => {
-    db.User.create(req.body)
-    .then
+router.post("/",(req,res)=>{
+    db.User.create(req.body).then(user=>{
+        req.session.user = {
+            id:user.id,
+            username:user.username,
+            email:user.email,
+        }
+        res.json(user);
+    }).catch(err=>{
+        console.log(err);
+        res.status(500).json(err);
+    })
 })
