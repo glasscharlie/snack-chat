b02bca1462a7a083e13521163903a7c8e78ccf24
Marcos Trejo <marcostrejo23@gmail.com>
2021-08-03 11:17:19 -0700

    pulling updated models

--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
 const { Model, DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt');
 const sequelize = require ('../config/connection');
 
 class User extends Model {}
@@ -23,23 +24,53 @@ User.init (
         email: {
             type:DataTypes.STRING,
             allowNull: false,
+            unique: true, 
+            validate: {
+                isEmail:true,
+            },
         },
         password: {
             type: DataTypes.STRING,
             allowNull: false,
+            validate: {
+                len: [1],
+            },
         }, 
         friendsList: {
             type: DataTypes.String, 
         }
     },
     {
+    hooks: {
+        beforeCreate: async (newUser) => {
+          try {
+            newUser.password = await bcrypt.hash(newUser.password, 10);
+            return newUser;
+          } catch (err) {
+            console.log(err);
+            return err;
+          }
+        },
+        beforeUpdate: async (updatedUser) => {
+          try {
+            updatedUser.password = await bcrypt.hash(
+              updatedUser.password,
+              10
+            );
+            return updatedUser;
+          } catch (err) {
+            console.log(err);
+            return err;
+          }
+        },
+    
         sequelize,
         timestamps: false,
         freezeTableName: true,
         underscored: true,
         modelName: 'user',
-
     }
+}   
 );
 
 module.exports = User;
