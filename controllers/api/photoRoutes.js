ec62a393068cca060d83cd91f4964fe0dcc11ec3
Charles Glass <charlie-glass@live.com>
2021-08-05 09:57:42 -0700

    added restaurant backend route

--- controllers/api/photoRoutes.js
+++ controllers/api/photoRoutes.js
@@ -3,8 +3,8 @@ const router = express.Router();
 const db = require('../../models');
 
 router.get("/",(req,res)=>{
-    db.Photos.findAll().then(users=>{
-       res.json(users);
+    db.Photos.findAll({include:[db.User]}).then(photos=>{
+       res.json(photos);
     }).catch(err=>{
         console.log(err);
         res.status(500).json(err);
@@ -18,7 +18,21 @@ router.post("/",(req,res)=>{
             user_id:photo.user_id,
             review:photo.review,
             url:photo.url,
+            restaurant:photo.restaurant
         }
+        res.json(photo);
+    }).catch(err=>{
+        console.log(err);
+        res.status(500).json(err);
+    })
+})
+
+router.get("/:restaurant",(req,res)=>{
+    db.Photos.findByPk(req.params.restaurant,{
+        include:[{
+            model:db.User
+        }]
+    }).then(photo=>{
         res.json(photo);
     }).catch(err=>{
         console.log(err);
